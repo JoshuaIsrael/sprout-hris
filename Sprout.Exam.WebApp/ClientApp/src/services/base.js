b86a54531cc3974f4b1b3d642b480f96fbe72c5c
Joshua Israel Albao <albao.joshuaisrael@gmail.com>
2023-03-25 16:12:25 +0800

    Fixed incorrect base config value

--- Sprout.Exam.WebApp/ClientApp/src/services/base.js
+++ Sprout.Exam.WebApp/ClientApp/src/services/base.js
@@ -3,37 +3,43 @@ import authService from '../components/api-authorization/AuthorizeService';
 const getBaseConfig = async () => {
   const token = await authService.getAccessToken();
   return {
-    ...(token ? {'Authorization': `Bearer ${token}`} : {}),
-    'Content-Type': 'application/json'
+    headers: {
+      ...(token ? {'Authorization': `Bearer ${token}`} : {}),
+      'Content-Type': 'application/json'
+    }
   }
 }
 
 export const postRequest = async (url, payload) => {
+  const baseConfig = await getBaseConfig();
   return await fetch(`api/${url}`, {
-    ...getBaseConfig(),
+    ...baseConfig,
     method: 'POST',
     body: JSON.stringify(payload)
   })
 }
 
 export const getRequest = async (url) => {
+  const baseConfig = await getBaseConfig();
   return await fetch(`api/${url}`, {
-    ...getBaseConfig(),
+    ...baseConfig,
     method: 'GET',
   });
 }
 
 export const putRequest = async (url, payload) => {
+  const baseConfig = await getBaseConfig();
   return await fetch(`api/${url}`, {
-    ...getBaseConfig(),
+    ...baseConfig,
     method: 'PUT',
     body: JSON.stringify(payload),
   });
 }
 
 export const deleteRequest = async (url) => {
+  const baseConfig = await getBaseConfig();
   return await fetch(`api/${url}`, {
-    ...getBaseConfig(),
+    ...baseConfig,
     method: 'DELETE',
   });
 }
